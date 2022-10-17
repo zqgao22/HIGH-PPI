eb895de22370465904b3a0b6bf9c0dd2c377e9ac
zqgao22 <103722116+zqgao22@users.noreply.github.com>
2022-10-17 14:58:06 +0800

    Add files via upload

---
+++ model_test.py
@@ -0,0 +1,233 @@
+import os
+import time
+import math
+import json
+import random
+import numpy as np
+import argparse
+import torch
+import torch.nn as nn
+
+from tqdm import tqdm
+from gnn_models_sag import GIN_Net2, ppi_model
+from gnn_data import GNN_DATA
+from gnn_models_sag import GIN_Net2
+from utils import Metrictor_PPI, print_file
+parser = argparse.ArgumentParser(description='HIGH-PPI_model_training')
+
+parser.add_argument('--ppi_path', default=None, type=str,
+                    help="ppi path")
+parser.add_argument('--pseq_path', default=None, type=str,
+                    help="protein sequence path")
+parser.add_argument('--vec_path', default='./vec5_CTC.txt', type=str,
+                    help='protein sequence vector path')
+parser.add_argument('--p_feat_matrix', default=None, type=str,
+                    help="protein feature matrix")
+parser.add_argument('--p_adj_matrix', default=None, type=str,
+                    help="protein adjacency matrix")
+parser.add_argument('--index_path', default=None, type=str,
+                    help='training and test PPI index')
+parser.add_argument('--model_path', default=None, type=str,
+                    help="path for trained model")
+
+def multi2big_x(x_ori):
+    x_cat = torch.zeros(1, 7)
+    x_num_index = torch.zeros(1553)
+    for i in range(1553):
+        x_now = torch.tensor(x_ori[i])
+        x_num_index[i] = torch.tensor(x_now.size(0))
+        x_cat = torch.cat((x_cat, x_now), 0)
+    return x_cat[1:, :], x_num_index
+
+def multi2big_batch(x_num_index):
+    num_sum = x_num_index.sum()
+    num_sum = num_sum.int()
+    batch = torch.zeros(num_sum)
+    count = 1
+    for i in range(1,1553):
+        zj1 = x_num_index[:i]
+        zj11 = zj1.sum()
+        zj11 = zj11.int()
+        zj22 = zj11 + x_num_index[i]
+        zj22 = zj22.int()
+        size1 = x_num_index[i]
+        size1 = size1.int()
+        tc = count * torch.ones(size1)
+        batch[zj11:zj22] = tc
+        test = batch[zj11:zj22]
+        count = count + 1
+    batch = batch.int()
+    return batch
+
+def multi2big_edge(edge_ori, num_index):
+    edge_cat = torch.zeros(2, 1)
+    edge_num_index = torch.zeros(1553)
+    for i in range(1553):
+        edge_index_p = edge_ori[i]
+        edge_index_p = np.asarray(edge_index_p)
+        edge_index_p = torch.tensor(edge_index_p.T)
+        edge_num_index[i] = torch.tensor(edge_index_p.size(1))
+        if i == 0:
+            offset = 0
+        else:
+            zj = torch.tensor(num_index[:i])
+            offset = zj.sum()
+        edge_cat = torch.cat((edge_cat, edge_index_p + offset), 1)
+    return edge_cat[:, 1:], edge_num_index
+
+
+def boolean_string(s):
+    if s not in {'False', 'True'}:
+        raise ValueError('Not a valid boolean string')
+    return s == 'True'
+
+
+def test(model, graph, test_mask, device,batch, p_x_all, p_edge_all):
+    valid_pre_result_list = []
+    valid_label_list = []
+
+    model.eval()
+
+    batch_size = 64
+
+    valid_steps = math.ceil(len(test_mask) / batch_size)
+    
+    valid_pre_result_list = []
+    valid_label_list = []
+    for step in tqdm(range(valid_steps)):
+        if step == valid_steps-1:
+            valid_edge_id = test_mask[step*batch_size:]
+        else:
+            valid_edge_id = test_mask[step*batch_size : step*batch_size + batch_size]
+
+        # output = model(graph.x, graph.edge_index, valid_edge_id)
+        output = model(batch, p_x_all, p_edge_all, graph.edge_index, valid_edge_id)
+        label = graph.edge_attr_1[valid_edge_id]
+        label = label.type(torch.FloatTensor).to(device)
+
+        m = nn.Sigmoid()
+        pre_result = (m(output) > 0.5).type(torch.FloatTensor).to(device)
+
+        valid_pre_result_list.append(pre_result.cpu().data)
+        valid_label_list.append(label.cpu().data)
+
+    valid_pre_result_list = torch.cat(valid_pre_result_list, dim=0)
+    valid_label_list = torch.cat(valid_label_list, dim=0)
+    metrics = Metrictor_PPI(valid_pre_result_list, valid_label_list)
+
+    metrics.show_result()
+
+    print_file("F1: {}".format(metrics.F1))
+    print_file("aupr: {}".format(metrics.Aupr))
+    print(valid_pre_result_list)
+    print(valid_label_list)
+
+def main():
+
+    args = parser.parse_args()
+
+    ppi_data = GNN_DATA(ppi_path=args.ppi_path)
+    ppi_data.get_feature_origin(pseq_path=args.pseq_path,
+                                vec_path=args.vec_path)
+
+
+    ppi_data.generate_data()
+
+    graph = ppi_data.data
+    temp = graph.edge_index.transpose(0, 1).numpy()
+    ppi_list = []
+
+    for edge in temp:
+        ppi_list.append(list(edge))
+
+    truth_edge_num = len(ppi_list) // 2
+    # fake_edge_num = len(ppi_data.fake_edge) // 2
+    fake_edge_num = 0
+    index_path = args.index_path
+    with open(index_path, 'r') as f:
+        index_dict = json.load(f)
+        f.close()
+    graph.train_mask = index_dict['train_index']
+
+    graph.val_mask = index_dict['valid_index']
+
+    print("train gnn, train_num: {}, valid_num: {}".format(len(graph.train_mask), len(graph.val_mask)))
+
+    node_vision_dict = {}
+    for index in graph.train_mask:
+        ppi = ppi_list[index]
+        if ppi[0] not in node_vision_dict.keys():
+            node_vision_dict[ppi[0]] = 1
+        if ppi[1] not in node_vision_dict.keys():
+            node_vision_dict[ppi[1]] = 1
+
+    for index in graph.val_mask:
+        ppi = ppi_list[index]
+        if ppi[0] not in node_vision_dict.keys():
+            node_vision_dict[ppi[0]] = 0
+        if ppi[1] not in node_vision_dict.keys():
+            node_vision_dict[ppi[1]] = 0
+    
+    vision_num = 0
+    unvision_num = 0
+    for node in node_vision_dict:
+        if node_vision_dict[node] == 1:
+            vision_num += 1
+        elif node_vision_dict[node] == 0:
+            unvision_num += 1
+    print("vision node num: {}, unvision node num: {}".format(vision_num, unvision_num))
+
+    test1_mask = []
+    test2_mask = []
+    test3_mask = []
+
+    for index in graph.val_mask:
+        ppi = ppi_list[index]
+        temp = node_vision_dict[ppi[0]] + node_vision_dict[ppi[1]]
+        if temp == 2:
+            test1_mask.append(index)
+        elif temp == 1:
+            test2_mask.append(index)
+        elif temp == 0:
+            test3_mask.append(index)
+    print("test1 edge num: {}, test2 edge num: {}, test3 edge num: {}".format(len(test1_mask), len(test2_mask), len(test3_mask)))
+
+    graph.test1_mask = test1_mask
+    graph.test2_mask = test2_mask
+    graph.test3_mask = test3_mask
+
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+    model = ppi_model()
+    model.to(device)
+
+    # model = GIN_Net2(in_len=2000, in_feature=13, gin_in_feature=256, num_layers=1, pool_size=3, cnn_hidden=1).to(device)
+    model_path = args.model_path
+    model.load_state_dict(torch.load(model_path)['state_dict'])
+
+    graph.to(device)
+
+
+
+
+
+    p_x_all = torch.load(args.p_feat_matrix)
+    # p_edge_all = np.load('/apdcephfs/share_1364275/kaithgao/edge_list_12.npy', allow_pickle=True)
+    p_edge_all = np.load(args.p_adj_matrix, allow_pickle=True)
+    p_x_all, x_num_index = multi2big_x(p_x_all)
+    # p_x_all = p_x_all[:,torch.arange(p_x_all.size(1))!=6] 
+    p_edge_all, edge_num_index = multi2big_edge(p_edge_all, x_num_index)
+
+
+    batch = multi2big_batch(x_num_index)+1
+
+
+
+
+
+
+
+    test(model, graph, graph.val_mask, device,batch, p_x_all, p_edge_all)
+    
+if __name__ == "__main__":
+    main()
