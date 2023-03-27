4c20e732ef3317f041e821500f38c37072d8bf2d
zqgao22 <103722116+zqgao22@users.noreply.github.com>
2023-03-27 18:29:57 +0800

    Update gnn_models_sag.py

--- gnn_models_sag.py
+++ gnn_models_sag.py
@@ -91,18 +91,22 @@ class GCN(nn.Module):
         self.conv1 = GCNConv(7, hidden)
         self.conv2 = GCNConv(hidden, hidden)
         self.conv3 = GCNConv(hidden, hidden)
+        self.conv4 = GCNConv(hidden, hidden)
   
         self.bn1 = nn.BatchNorm1d(hidden)
         self.bn2 = nn.BatchNorm1d(hidden)
         self.bn3 = nn.BatchNorm1d(hidden)
+        self.bn4 = nn.BatchNorm1d(hidden)
 
         self.sag1 = SAGPooling(hidden,0.5)
-        self.sag2 = SAGPooling(hidden,0.1)
-        
+        self.sag2 = SAGPooling(hidden,0.5)
+        self.sag3 = SAGPooling(hidden,0.5)
+        self.sag4 = SAGPooling(hidden,0.5)
 
         self.fc1 = nn.Linear(hidden, hidden)
         self.fc2 = nn.Linear(hidden, hidden)
         self.fc3 = nn.Linear(hidden, hidden)
+        self.fc4 = nn.Linear(hidden, hidden)
 
         self.dropout = nn.Dropout(0.5)
         for param in self.parameters():
@@ -114,18 +118,39 @@ class GCN(nn.Module):
         x = self.fc1(x)
         x = F.relu(x) 
         x = self.bn1(x)
+        y = self.sag1(x, edge_index, batch = batch)
+        x = y[0]
+        batch = y[3]
+        edge_index = y[1] 
 
         x = self.conv2(x, edge_index)
         x = self.fc2(x)
         x = F.relu(x) 
         x = self.bn2(x)
-        
-        y = self.sag1(x, edge_index, batch = batch)
+        y = self.sag2(x, edge_index, batch = batch)
         x = y[0]
         batch = y[3]
-        edge_index = y[1] 
+        edge_index = y[1]  
         
-        y = self.sag2(x, edge_index, batch = batch)
+        x = self.conv3(x, edge_index)
+        x = self.fc3(x)
+        x = F.relu(x) 
+        x = self.bn3(x)
+        y = self.sag3(x, edge_index, batch = batch)
+        x = y[0]
+        batch = y[3]
+        edge_index = y[1]
+
+        x = self.conv4(x, edge_index)
+        x = self.fc4(x)
+        x = F.relu(x) 
+        x = self.bn4(x)
+        y = self.sag4(x, edge_index, batch = batch)
+        x = y[0]
+        batch = y[3]
+        edge_index = y[1]
+
+        # y = self.sag4(x, edge_index, batch = batch)
 
         return global_mean_pool(y[0], y[3])
         # return y[0]
